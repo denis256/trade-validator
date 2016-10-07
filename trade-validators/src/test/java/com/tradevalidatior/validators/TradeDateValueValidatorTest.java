0d45b3ef9fa495c895b4da24945fb2d1de8106d4
Denis O <denis@universal-development.com>
2016-10-07 20:48:14 +0300

    Tests for "value date cannot be before trade date"

---
+++ trade-validators/src/test/java/com/tradevalidatior/validators/TradeDateValueValidatorTest.java
@@ -0,0 +1,71 @@
+package com.tradevalidatior.validators;
+
+
+import com.tradevalidator.model.Trade;
+import com.tradevalidator.model.ValidationError;
+import com.tradevalidator.model.ValidationResult;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.stream.Collectors;
+
+import static com.tradevalidator.model.Trade.newTrade;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+
+public class TradeDateValueValidatorTest {
+
+    private Trade trade;
+    private SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+    private TradeDateValueValidator tradeDateValueValidator = new TradeDateValueValidator();
+
+    @Before
+    public void before() {
+        trade = newTrade();
+    }
+
+    @Test
+    public void test_TradeDateValueValidator_Valid_Path() throws ParseException {
+        trade.setTradeDate(dateFormatter.parse("2016-08-10"));
+        trade.setValueDate(dateFormatter.parse("2016-08-20"));
+
+        ValidationResult result = tradeDateValueValidator.validate(trade);
+
+        assertThat(result, is(not(nullValue())));
+        assertThat(result.hasErrors(), is(false));
+    }
+
+    @Test
+    public void test_TradeDateValueValidator_Negative_path() throws ParseException {
+        trade.setTradeDate(dateFormatter.parse("2016-08-20"));
+        trade.setValueDate(dateFormatter.parse("2016-08-01"));
+
+        ValidationResult result = tradeDateValueValidator.validate(trade);
+
+        assertThat(result, is(not(nullValue())));
+        assertThat(result.hasErrors(), is(true));
+        assertThat(result.errors().size(), is(1));
+
+        ValidationError firstError = result.errors().stream().findFirst().get();
+        assertThat(firstError.field(), is("valueDate"));
+    }
+
+    @Test
+    public void test_TradeDateValueValidator_nullvalue() {
+        trade.setTradeDate(null);
+        trade.setValueDate(null);
+
+        ValidationResult result = tradeDateValueValidator.validate(trade);
+
+        assertThat(result, is(not(nullValue())));
+        assertThat(result.hasErrors(), is(true));
+        assertThat(result.errors().size(), is(2));
+        assertThat(result.errors().stream().map(ValidationError::field).collect(Collectors.toList()), containsInAnyOrder("valueDate", "tradeDate"));
+    }
+}
