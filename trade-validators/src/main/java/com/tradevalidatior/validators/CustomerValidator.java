8cab3052bb1d23b9a7039c646513e6ace34e9185
Denis O <denis@universal-development.com>
2016-10-07 21:12:50 +0300

    Customer validation initialization

--- trade-validators/src/main/java/com/tradevalidatior/validators/CustomerValidator.java
+++ trade-validators/src/main/java/com/tradevalidatior/validators/CustomerValidator.java
@@ -5,22 +5,29 @@ import com.tradevalidator.model.Trade;
 import com.tradevalidator.model.ValidationError;
 import com.tradevalidator.model.ValidationResult;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.tradevalidator.model.ValidationResult.newValidationResult;
+import static com.tradevalidator.model.ValidationError.validationError;
 
 public class CustomerValidator implements TradeValidator {
 
-    private Collection<String> validCustomers = new ArrayList<>();
+    private Set<String> validCustomers = new HashSet<>();
+
+    public CustomerValidator() {
+        validCustomers.add("PLUTO1");
+        validCustomers.add("PLUTO2");
+    }
 
     @Override
     public ValidationResult validate(Trade trade) {
 
-        ValidationResult validationResult = new ValidationResult();
+        ValidationResult validationResult = newValidationResult();
 
         if (validCustomers.contains(trade.getCustomer())) {
-            validationResult.withError(new ValidationError().field("customer").message("Customer is not in approved list"));
+            validationResult.withError(validationError().field("customer").message("Customer is not in approved list"));
         }
-
 
         return validationResult;
     }
