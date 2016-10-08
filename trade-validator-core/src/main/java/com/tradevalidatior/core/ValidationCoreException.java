971de858cebe162b51c357c95e18c4a84a6dba29
Denis O <denis@universal-development.com>
2016-10-08 07:58:09 +0300

    Added core validation exception

---
+++ trade-validator-core/src/main/java/com/tradevalidatior/core/ValidationCoreException.java
@@ -0,0 +1,22 @@
+package com.tradevalidatior.core;
+
+/**
+ * Exception which may appear during validation evaluations
+ */
+public class ValidationCoreException extends RuntimeException {
+
+    public ValidationCoreException() {
+    }
+
+    public ValidationCoreException(String message) {
+        super(message);
+    }
+
+    public ValidationCoreException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationCoreException(Throwable cause) {
+        super(cause);
+    }
+}
