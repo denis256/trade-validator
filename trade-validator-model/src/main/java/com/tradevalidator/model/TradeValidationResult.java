d02a29b54cc51650305665de6f7351dd2321a9bd
Denis O <denis@universal-development.com>
2016-10-08 12:45:18 +0300

    Trade validation result

---
+++ trade-validator-model/src/main/java/com/tradevalidator/model/TradeValidationResult.java
@@ -0,0 +1,46 @@
+package com.tradevalidator.model;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Model object for storing trade validation results, holds trade and collection of invalid fields with messages
+ */
+public class TradeValidationResult {
+
+    private Trade trade;
+
+    private Map<String, Collection<String>> invalidFields = new ConcurrentHashMap<>();
+
+    public static TradeValidationResult tradeValidationResult() {
+        return  new TradeValidationResult();
+    }
+
+    public TradeValidationResult addInvalidField(String field, String message) {
+
+        if (!invalidFields.containsKey(field)) {
+            invalidFields.put(field, new ConcurrentLinkedQueue<>());
+        }
+        invalidFields.get(field).add(message);
+        return this;
+    }
+
+
+    public Trade getTrade() {
+        return trade;
+    }
+
+    public void setTrade(Trade trade) {
+        this.trade = trade;
+    }
+
+    public Map<String, Collection<String>> getInvalidFields() {
+        return invalidFields;
+    }
+
+    public void setInvalidFields(Map<String, Collection<String>> invalidFields) {
+        this.invalidFields = invalidFields;
+    }
+}
