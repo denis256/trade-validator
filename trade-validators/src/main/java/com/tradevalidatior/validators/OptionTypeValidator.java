33b9c4a652fccdf4a6cb2f609afde5e0138f5fc5
Denis O <denis@universal-development.com>
2016-10-08 07:53:23 +0300

    Added american style options checking in validator

--- trade-validators/src/main/java/com/tradevalidatior/validators/OptionTypeValidator.java
+++ trade-validators/src/main/java/com/tradevalidatior/validators/OptionTypeValidator.java
@@ -43,12 +43,39 @@ public class OptionTypeValidator implements TradeValidator {
 
             // expiry date and premium date shall be before delivery date
 
+            // deliveryDate check
+
             if (trade.getExpiryDate() == null) {
-                validationResult.withError(validationError().field("style").message("Option style is not valid"));
+                validationResult.withError(validationError().field("expiryDate").message("expiry date is missing"));
+            } else {
+                if (!trade.getExpiryDate().before(trade.getDeliveryDate())) {
+                    validationResult.withError(validationError().field("expiryDate").message("expiry date should be before delivery date"));
+                }
+            }
+
+            // premium date check
+            if (trade.getPremiumDate() == null) {
+                validationResult.withError(validationError().field("premiumDate").message("premium date is missing"));
+            } else {
+                if (!trade.getPremiumDate().before(trade.getDeliveryDate())) {
+                    validationResult.withError(validationError().field("premiumDate").message("premium date should be before delivery date"));
+                }
             }
 
             if (americanStyles.contains(trade.getStyle())) { // American option style will have in addition the excerciseStartDate, which has to be after the trade date but before the expiry date
 
+                if (trade.getExcerciseStartDate() == null) {
+                    validationResult.withError(validationError().field("excerciseStartDate").message("excerciseStartDate is missing"));
+                } else {
+
+                    if (!trade.getExcerciseStartDate().after(trade.getTradeDate())) { // trade date check
+                        validationResult.withError(validationError().field("excerciseStartDate").message("excerciseStartDate should be after trade date"));
+                    }
+
+                    if (!trade.getExcerciseStartDate().before(trade.getExpiryDate())) { // expiry date check
+                        validationResult.withError(validationError().field("excerciseStartDate").message("excerciseStartDate should be before trade date"));
+                    }
+                }
 
 
             }
