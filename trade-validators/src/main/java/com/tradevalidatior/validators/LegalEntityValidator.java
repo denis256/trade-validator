177ca04841a77503c2d8f19ce38fdd73e805ca15
Denis O <denis@universal-development.com>
2016-10-08 07:35:34 +0300

    Legal entity validator

---
+++ trade-validators/src/main/java/com/tradevalidatior/validators/LegalEntityValidator.java
@@ -0,0 +1,36 @@
+package com.tradevalidatior.validators;
+
+import com.tradevalidatior.validator.TradeValidator;
+import com.tradevalidator.model.Trade;
+import com.tradevalidator.model.ValidationResult;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.tradevalidator.model.ValidationResult.newValidationResult;
+import static com.tradevalidator.model.ValidationError.validationError;
+
+
+/**
+ * Only one legal entity is used: CS Zurich
+ */
+public class LegalEntityValidator implements TradeValidator {
+
+    private Set<String> legalEntities = new HashSet<>();
+
+    public LegalEntityValidator() {
+        legalEntities.add("CS Zurich");
+    }
+
+    @Override
+    public ValidationResult validate(Trade trade) {
+
+        ValidationResult validationResult = ValidationResult.newValidationResult();
+
+        if(!legalEntities.contains(trade.getLegalEntity())) {
+            validationResult.withError(validationError().field("legalEntity").message("Legal entity is invalid"));
+        }
+
+        return validationResult;
+    }
+}
