5413ec5a9184c20e56198881c4249373ba1b765e
Denis O <denis@universal-development.com>
2016-10-08 10:00:18 +0300

    Test case when value date is missing

--- trade-validators/src/test/java/com/tradevalidator/validators/SpotForwardValidatorTest.java
+++ trade-validators/src/test/java/com/tradevalidator/validators/SpotForwardValidatorTest.java
@@ -2,6 +2,7 @@ package com.tradevalidator.validators;
 
 
 import com.tradevalidator.model.Trade;
+import com.tradevalidator.model.ValidationError;
 import com.tradevalidator.model.ValidationResult;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.stream.Stream;
 
 import static com.tradevalidator.model.Trade.newTrade;
 import static org.hamcrest.CoreMatchers.is;
@@ -32,8 +34,8 @@ public class SpotForwardValidatorTest {
 
     @Test
     public void test_Spot_postive_path() {
-        Date validSpotValiDate = DateUtils.addDays(spotForwardValidator.getTodayDate(), 2);
-        trade.setValueDate(validSpotValiDate);
+        Date validSpotDate = DateUtils.addDays(spotForwardValidator.getTodayDate(), 2);
+        trade.setValueDate(validSpotDate);
         trade.setType("Spot");
 
         ValidationResult result = spotForwardValidator.validate(trade);
@@ -45,8 +47,8 @@ public class SpotForwardValidatorTest {
     @Test
     public void test_Forward_postivie_path() {
 
-        Date validSpotValiDate = DateUtils.addDays(spotForwardValidator.getTodayDate(), 5);
-        trade.setValueDate(validSpotValiDate);
+        Date validForwardDate = DateUtils.addDays(spotForwardValidator.getTodayDate(), 5);
+        trade.setValueDate(validForwardDate);
         trade.setType("Forward");
 
 
@@ -54,6 +56,26 @@ public class SpotForwardValidatorTest {
 
         assertThat(result, is(not(nullValue())));
         assertThat(result.errors(), is(empty()));
+
+    }
+
+    @Test
+    public  void test_SpotForward_missing_value_date() {
+        Stream.of("Spot", "Forward").forEach( type -> {
+            trade.setValueDate(null);
+            trade.setType(type);
+
+            ValidationResult result = spotForwardValidator.validate(trade);
+
+            assertThat(result, is(not(nullValue())));
+            assertThat(result.hasErrors(), is(true));
+            assertThat(result.errors().size(), is(1));
+
+            ValidationError firstError = result.errors().stream().findFirst().get();
+            assertThat(firstError.field(), is("valueDate"));
+            assertThat(firstError.message(), is("valueDate is missing"));
+        });
+
 
     }
 
