e3b5728df9c029d837cbc91f4144d6082872522e
Denis O <denis@universal-development.com>
2016-10-08 08:17:58 +0300

    JMinix jmx page mapping

--- trade-validator-app/src/main/java/com/tradevalidator/TradeValidatorAppApplication.java
+++ trade-validator-app/src/main/java/com/tradevalidator/TradeValidatorAppApplication.java
@@ -1,14 +1,27 @@
 package com.tradevalidator;
 
+import ch.qos.logback.classic.ViewStatusMessagesServlet;
+import org.jminix.console.servlet.MiniConsoleServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 @SpringBootApplication
 @ComponentScan("com.tradevalidator")
-public class TradeValidatorAppApplication {
+public class TradeValidatorAppApplication implements ServletContextInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradeValidatorAppApplication.class, args);
 	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.addServlet("ViewStatusMessages", ViewStatusMessagesServlet.class).addMapping("/logs");
+		servletContext.addServlet("JmxMiniConsoleServlet", MiniConsoleServlet.class).addMapping("/jmx/*");
+	}
+
 }
