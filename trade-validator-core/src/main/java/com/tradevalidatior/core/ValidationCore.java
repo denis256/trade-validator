233cc725c5204749d5aa479d4a02bed810f9a9cd
Denis O <denis@universal-development.com>
2016-10-08 08:01:44 +0300

    Added core shutdown sequence

--- trade-validator-core/src/main/java/com/tradevalidatior/core/ValidationCore.java
+++ trade-validator-core/src/main/java/com/tradevalidatior/core/ValidationCore.java
@@ -3,10 +3,13 @@ package com.tradevalidatior.core;
 import com.tradevalidatior.validator.TradeValidator;
 import com.tradevalidator.model.Trade;
 import com.tradevalidator.model.ValidationResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 /**
@@ -14,7 +17,12 @@ import java.util.stream.Collectors;
  */
 public class ValidationCore {
 
+    private static Logger LOG = LoggerFactory.getLogger(ValidationCore.class);
+
+
     private Collection<TradeValidator> tradeValidators;
+
+    private AtomicBoolean shutdown = new AtomicBoolean(false);
 
     public ValidationCore() {
         tradeValidators = new ArrayList<>();
@@ -27,6 +35,10 @@ public class ValidationCore {
      */
     public List<ValidationResult> validateTrade(Trade trade) {
 
+        if (shutdown.get()) {
+            throw new ValidationCoreException("ValidationCore is shutting down");
+        }
+
         return tradeValidators.parallelStream()
                 .map( validator ->  validator.validate(trade))
                 .filter(validationResult -> validationResult.hasErrors())
@@ -38,5 +50,19 @@ public class ValidationCore {
         return this;
     }
 
+    public boolean fetchShutdownStatus() {
+        return shutdown.get();
+    }
 
+    public ValidationCore shutdown() {
+        LOG.info("Shutdown initiated");
+        shutdown.set(true);
+        return this;
+    }
+
+    public ValidationCore cancelShutdown() {
+        LOG.info("Shutdown canceled");
+        shutdown.set(false);
+        return this;
+    }
 }
